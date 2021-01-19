a529cf0d3af17dcc17940c5ad3afff8e9a5a115e
Hoangtu9999 <58470170+Hoangtu9999@users.noreply.github.com>
2021-01-19 17:35:12 +0700

    tuup

---
+++ c0720g1-vaccine/src/app/vaccination-history/immunization-history-feedback/immunization-history-feedback.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-immunization-history-feedback',
+  templateUrl: './immunization-history-feedback.component.html',
+  styleUrls: ['./immunization-history-feedback.component.scss']
+})
+export class ImmunizationHistoryFeedbackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
