964708a70334bce61aeab6925f043b6c34bc7c86
winback1000 <68835331+winback1000@users.noreply.github.com>
2021-01-20 02:20:16 +0700

    Update patient-routing.module.ts

--- c0720g1-vaccine/src/app/patient/patient-routing.module.ts
+++ c0720g1-vaccine/src/app/patient/patient-routing.module.ts
@@ -1,37 +1,25 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {ListPatientComponent} from './list-patient/list-patient.component';
 import {EditPatientComponent} from './edit-patient/edit-patient.component';
 import {DeletePatientComponent} from './delete-patient/delete-patient.component';
+import {PatientCreateComponent} from './patient-create/patient-create.component';
 
 const routes: Routes = [
   {path: 'patient', redirectTo: 'patient/list', pathMatch: 'full'},
   {path: 'patient/list', component: ListPatientComponent},
   {path: 'patient/edit', component: EditPatientComponent},
   {path: 'patient/list/delete', component: DeletePatientComponent},
+  {path: 'patient/create', component: PatientCreateComponent}
 ];
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+    declarations: [],
+    imports: [
+      CommonModule,
+      RouterModule.forChild(routes)
+    ]
   exports: [RouterModule]
 })
 export class PatientRoutingModule {
 }
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {RouterModule, Routes} from '@angular/router';
-import {PatientCreateComponent} from './patient-create/patient-create.component';
-
-
-
-
-const routes: Routes = [
-  {path: 'patient/create', component: PatientCreateComponent}
-];
-@NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes)
-  ]
-})
-export class PatientRoutingModule { }
