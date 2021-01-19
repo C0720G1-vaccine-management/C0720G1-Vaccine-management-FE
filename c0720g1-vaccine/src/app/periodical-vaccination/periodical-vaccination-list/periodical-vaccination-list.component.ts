d767b051ac9b142ec00828c450b5aa0d981fba94
DESKTOP-J2BIJDF\antra <antran264@gmail.com>
2021-01-20 02:46:44 +0700

    Khoa update periodical-vaccination module

---
+++ c0720g1-vaccine/src/app/periodical-vaccination/periodical-vaccination-list/periodical-vaccination-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-periodical-vaccination-list',
+  templateUrl: './periodical-vaccination-list.component.html',
+  styleUrls: ['./periodical-vaccination-list.component.scss']
+})
+export class PeriodicalVaccinationListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
