aeed3d09e0cff0acb5b32cb72c003852e5e96085
quoctrung1012 <68835471+quoctrung1012@users.noreply.github.com>
2021-01-19 18:14:18 +0700

    Delete IVaccinationTransaction.ts

--- c0720g1-vaccine/src/app/entity/IVaccinationTransaction.ts
+++
@@ -1,9 +0,0 @@
-import {IVaccinationHistory} from './IVaccinationHistory';
-
-export interface IVaccinationTransaction {
-  vaccinationTransactionId: number;
-  price: number;
-  quantity: number;
-
-  vaccinationHistory: IVaccinationHistory;
-}
