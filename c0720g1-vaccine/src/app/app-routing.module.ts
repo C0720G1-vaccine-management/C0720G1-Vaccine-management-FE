6dee110f2fbadb07082aecafc51e85e1cb00acb8
nvankhanh97 <nvankhanh97@gmail.com>
2021-01-19 22:33:49 +0700

    up

--- c0720g1-vaccine/src/app/app-routing.module.ts
+++ c0720g1-vaccine/src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { VaccineTransactionManagementModule } from './vaccine-transaction-management/vaccine-transaction-management.module';
-
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), VaccineTransactionManagementModule],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
