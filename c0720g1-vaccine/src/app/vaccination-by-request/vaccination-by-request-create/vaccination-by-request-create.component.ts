5894f6ef9060da6433e3c4ea9a1a339af3d83bf2
tran cong phuoc <teosake.1999@gmail.com>
2021-01-19 21:17:46 +0700

    phuoc

---
+++ c0720g1-vaccine/src/app/vaccination-by-request/vaccination-by-request-create/vaccination-by-request-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vaccination-by-request-create',
+  templateUrl: './vaccination-by-request-create.component.html',
+  styleUrls: ['./vaccination-by-request-create.component.scss']
+})
+export class VaccinationByRequestCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
