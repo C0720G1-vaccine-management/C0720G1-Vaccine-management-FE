19db127309368c51bb7bccdb038a365f50135fc8
quoctrung1012 <68835471+quoctrung1012@users.noreply.github.com>
2021-01-21 03:50:50 +0700

    trung gui bai FE (#28)

---
+++ c0720g1-vaccine/src/app/periodical-vaccination-manager/periodical-vaccination-manager-create/periodical-vaccination-manager-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-periodical-vaccination-manager-create',
+  templateUrl: './periodical-vaccination-manager-create.component.html',
+  styleUrls: ['./periodical-vaccination-manager-create.component.scss']
+})
+export class PeriodicalVaccinationManagerCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ c0720g1-vaccine/src/app/periodical-vaccination-manager/periodical-vaccination-manager-edit/periodical-vaccination-manager-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-periodical-vaccination-manager-edit',
+  templateUrl: './periodical-vaccination-manager-edit.component.html',
+  styleUrls: ['./periodical-vaccination-manager-edit.component.scss']
+})
+export class PeriodicalVaccinationManagerEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
