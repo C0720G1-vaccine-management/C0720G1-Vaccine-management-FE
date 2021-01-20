bd6b4addfea25934ca7bdd3a5f7d7cad24a2c96f
thanhluyen2011 <68835939+thanhluyen2011@users.noreply.github.com>
2021-01-21 03:49:41 +0700

    luyen up (#26)
    Co-authored-by: luyen <luyen2011@gmail.com>

---
+++ c0720g1-vaccine/src/app/registered-for-vaccination/registered-for-vaccination.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RegisteredForVaccinationRoutingModule} from "./registered-for-vaccination-routing.module";
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RegisteredForVaccinationRoutingModule
+  ]
+})
+export class RegisteredForVaccinationModule { }
