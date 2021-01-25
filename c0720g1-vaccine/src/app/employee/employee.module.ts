c7bb5d1be31d0ecb2d83466b6e3dc4195da243f9
luyen <luyen2011@gmail.com>
2021-01-25 18:48:39 +0700

    luyen up

--- c0720g1-vaccine/src/app/employee/employee.module.ts
+++ c0720g1-vaccine/src/app/employee/employee.module.ts
@@ -6,15 +6,19 @@ import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
 import { EmployeeCreateComponent } from './employee-create/employee-create.component';
 import {EmployeeRoutingModule} from './employee-routing.module';
 import {ReactiveFormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ToastrModule} from "ngx-toastr";
 
 
 @NgModule({
   declarations: [EmployeeListComponent, EmployeeEditComponent, EmployeeCreateComponent],
-    imports: [
-        CommonModule,
-        RouterModule,
-        EmployeeRoutingModule,
-        ReactiveFormsModule
-    ]
+  imports: [
+    CommonModule,
+    RouterModule,
+    EmployeeRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
+  ]
 })
 export class EmployeeModule { }
--- c0720g1-vaccine/src/app/service/vaccination-history.service.ts
+++ c0720g1-vaccine/src/app/service/vaccination-history.service.ts
@@ -5,13 +5,26 @@ import {HttpClient} from "@angular/common/http";
   providedIn: 'root'
 })
 export class VaccinationHistoryService {
-  private url = "http://localhost:8080/api";
+  private url = "http://localhost:8080/api/public";
   private header: any;
   constructor(private httpClient: HttpClient) {
     this.header = new Headers( {'Content-Type' : 'application/context'})
   }
 
-  getListPeriodicVaccination(page: number, name: string, status: boolean) {
-    return this.httpClient.get<any>(this.url + '/periodic-vaccination/list?name='+ name + '&status='+status + '&page=' + page);
+  /** LuyenNT code
+   * @param page
+   * @param name
+   * @param status
+   */
+  searchPeriodicVaccination(page: number, name: string, status: boolean) {
+    return this.httpClient.get<any>(this.url + '/periodic-vaccination/search?name='+ name + '&status='+status + '&page=' + page);
+  }
+
+  /** LuyenNT code
+   * @param page
+   * @param name
+   */
+  getListPeriodicVaccination(page: number, name: string) {
+    return this.httpClient.get<any>(this.url + '/periodic-vaccination/list?name='+ name + '&page=' + page);
   }
 }
