2b493ca1cdc215dc2bc1a94b74389279b3fb09ae
tin <vtt1801199@gmail.com>
2021-01-25 16:05:28 +0700

    tin up

--- c0720g1-vaccine/src/app/vaccine/vaccine.service.ts
+++ c0720g1-vaccine/src/app/vaccine/vaccine.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {IVaccineDTO} from "../entity/IVaccineDTO";
+import {ICreateDTO} from "../entity/ICreateDTO";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class VaccineService {
-  public API: string = "http://localhost:8081/api/";
+  public API: string = "http://localhost:8081/api/public/";
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json'
@@ -17,11 +19,45 @@ export class VaccineService {
     private http : HttpClient
   ) { }
 
-  getAllVaccine(): Observable<IVaccineDTO[]>{
-    return this.http.get<IVaccineDTO[]>(this.API + 'vaccines')
+
+  /**
+   * TinVT
+   * get all vaccineDTO
+   * @return
+   */
+  getAllVaccine(index: number): Observable<IVaccineDTO[]>{
+    return this.http.get<IVaccineDTO[]>(this.API + 'vaccines?index='+index)
   }
 
+
+  /**
+   * TinVT
+   * get all vaccineDTO
+   * @return
+   */
+  getAllVaccineNotPagination(): Observable<IVaccineDTO[]>{
+    return this.http.get<IVaccineDTO[]>(this.API + 'vaccines-not-pagination')
+  }
+
+
+
+  /**
+   * TinVT
+   * search vaccineDTO
+   * @return
+   */
   search(nameVaccine:string, typeVaccine:string, originVaccine:string, statusVaccine:string): Observable<IVaccineDTO[]>{
     return this.http.get<IVaccineDTO[]>(this.API + 'search?nameVaccine='+nameVaccine+'&typeVaccine='+typeVaccine+'&originVaccine='+originVaccine+'&statusVaccine='+statusVaccine)
+  }
+
+
+
+  /**
+   * TinVT
+   * create new vaccineDTO
+   * @return
+   */
+  createVaccineDTO(vaccine: ICreateDTO): Observable<ICreateDTO> {
+    return this.http.post<ICreateDTO>(this.API + 'vaccine-create/', JSON.stringify(vaccine), this.httpOptions)
   }
 }
