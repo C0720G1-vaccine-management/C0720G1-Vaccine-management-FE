54d25966304d37228c1c992b3ae3ad3a7b1f3a33
PhucDuy12061997 <nguyenphucduy12061997@gmail.com>
2021-01-25 18:19:19 +0700

    duy update FE

--- c0720g1-vaccine/src/app/app.module.ts
+++ c0720g1-vaccine/src/app/app.module.ts
@@ -5,33 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
-import {VaccineModule} from "./vaccine/vaccine.module";
-import {SecurityModule} from "./security/security.module";
-import {VaccinePriceManagementModule} from './vaccine-price-management/vaccine-price-management.module';
-import {ToastrModule} from "ngx-toastr";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {VaccinationByRequestModule} from "./vaccination-by-request/vaccination-by-request.module";
-import {HttpClientModule} from "@angular/common/http";
-import {PeriodicalVaccinationModule} from "./periodical-vaccination/periodical-vaccination.module";
-
+import {PatientModule} from './patient/patient.module';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     AppRoutingModule,
-    VaccineModule,
-    SecurityModule,
-    VaccinePriceManagementModule,
-    ToastrModule.forRoot(),
-    VaccinationByRequestModule,
-    HttpClientModule,
-    PeriodicalVaccinationModule,
+    PatientModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
