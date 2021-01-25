70e127097a12152836921f40fdc7e297cfa36e0c
nvankhanh97 <nvankhanh97@gmail.com>
2021-01-25 17:24:16 +0700

    khanh up 25/1

---
+++ c0720g1-vaccine/src/app/vaccine-transaction-management/edit-vaccine-transaction/edit-vaccine-transaction.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-vaccine-transaction',
+  templateUrl: './edit-vaccine-transaction.component.html',
+  styleUrls: ['./edit-vaccine-transaction.component.scss']
+})
+export class EditVaccineTransactionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
