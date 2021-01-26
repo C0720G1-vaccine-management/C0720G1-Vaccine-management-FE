7604664c7e91c171722f1aa0dc371619da917b6d
vothanhtin1997 <67451309+vothanhtin1997@users.noreply.github.com>
2021-01-26 15:39:51 +0700

    Vo thanh tin (#62)
    * tin up
    
    * tin up
    
    Co-authored-by: tin <vtt1801199@gmail.com>

---
+++ c0720g1-vaccine/src/app/entity/ICreateDTO.ts
@@ -0,0 +1,14 @@
+export interface ICreateDTO {
+  nameVaccine: string;
+  typeVaccine: string;
+  dateRecieve: string;
+  licenseCode: string;
+  orrigin: string;
+  provider: string;
+  unitPrice: number;
+  dosage: number;
+  quantity: number;
+  expired: string;
+  maintenance: string
+  age: string
+}
--- c0720g1-vaccine/src/app/vaccine/vaccine-create/vaccine-create.component.ts
+++ c0720g1-vaccine/src/app/vaccine/vaccine-create/vaccine-create.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {VaccineService} from "../vaccine.service";
+import {Router} from "@angular/router";
+import {AlertService} from "../alert.service";
 
 @Component({
   selector: 'app-vaccine-create',
@@ -6,11 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vaccine-create.component.scss']
 })
 export class VaccineCreateComponent implements OnInit {
+  formCreateVaccine: FormGroup;
 
-  constructor() { }
+
+  constructor(private vaccineService:VaccineService,
+              private router: Router,
+              private alertService: AlertService) {
+  }
 
   ngOnInit(): void {
-    console.log("hello")
+    this.formCreateVaccine = new FormGroup({
+      nameVaccine: new FormControl('',[Validators.required]),
+      typeVaccine: new FormControl('',[Validators.required]),
+      dateRecieve: new FormControl('',[Validators.required]),
+      licenseCode: new FormControl('',[Validators.required]),
+      orrigin: new FormControl('',[Validators.required]),
+      provider: new FormControl('',[Validators.required]),
+      unitPrice: new FormControl('',[Validators.required]),
+      dosage: new FormControl('',[Validators.required]),
+      quantity: new FormControl('',[Validators.required]),
+      expired: new FormControl('',[Validators.required]),
+      maintenance : new FormControl('',[Validators.required]),
+      age : new FormControl('',[Validators.required]),
+    })
+  }
+
+  save() {
+    console.log(this.formCreateVaccine.value);
+    this.vaccineService.createVaccineDTO(this.formCreateVaccine.value).subscribe(()=>{
+      this.router.navigateByUrl('vaccine-list').then(r => this.alertService.showMessage("Thêm mới thành công!"));
+    })
   }
 
 }
